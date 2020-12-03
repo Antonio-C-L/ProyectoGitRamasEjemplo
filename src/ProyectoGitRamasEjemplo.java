a59fb82a58bf00e320b440d93f1e28bda80c3ea6
Antonio-C-L <75076928+Antonio-C-L@users.noreply.github.com>
2020-12-03 10:59:18 +0100

    hello -> wola

--- src/ProyectoGitRamasEjemplo.java
+++ src/ProyectoGitRamasEjemplo.java
@@ -1,11 +1,11 @@
 
 public class ProyectoGitRamasEjemplo {
 	public static void imprimeSaludo() { 
-System.out.println(“Hei”);	
+System.out.println("Hei");	
 }
 
 	public static void main(String[] args) {
-		System.out.println("Hello mundo");
+		System.out.println("Wola mundo");
 	}
 
 }
